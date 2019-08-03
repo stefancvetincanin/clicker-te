cc7b224a3892fe01a53ae5edd74bb3c9e38f57dd
stefancvetincanin <stefancvetincanin@gmail.com>
2019-08-03 18:58:06 +0200

    Updates

---
+++ precache-manifest.15265776a2597c06cdf5772ad2d9288f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b50f9e3ee04b464948fc28ae7490f1e",
+    "url": "/clicker-te/index.html"
+  },
+  {
+    "revision": "97a019bfa4c0b0a2e4c7",
+    "url": "/clicker-te/static/css/main.ff56d422.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicker-te/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "97a019bfa4c0b0a2e4c7",
+    "url": "/clicker-te/static/js/main.ef600932.chunk.js"
+  },
+  {
+    "revision": "3c9af99df9b998307dc9",
+    "url": "/clicker-te/static/js/runtime~main.994c21bb.js"
+  },
+  {
+    "revision": "7ba725daacaf4be4af44f22da092452b",
+    "url": "/clicker-te/static/media/loader.7ba725da.gif"
+  }
+]);
